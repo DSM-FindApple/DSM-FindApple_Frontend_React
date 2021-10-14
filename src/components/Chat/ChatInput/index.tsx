aa4c53ecc70ce91266358d50be90128a8d19b3ce
oajh04 <cloudtubekr@naver.com>
2021-10-14 20:21:24 +0900

    ⚡️ :: ChatInput index

--- src/components/Chat/ChatInput/index.tsx
+++ src/components/Chat/ChatInput/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import * as S from './styles'
+import { BsImage } from 'react-icons/bs'
+import { AiOutlineSend } from 'react-icons/ai'
 
 const ChatInput = () => {
   return (
     <>
-        <div>
-            
-        </div>
+        <S.ChatInputBox>
+          <S.InputBox>
+            <BsImage />
+            <S.ChatInput />
+          </S.InputBox>
+          <S.SendChatButton>
+            <AiOutlineSend/>
+          </S.SendChatButton>
+        </S.ChatInputBox>
     </>
   );
 }
