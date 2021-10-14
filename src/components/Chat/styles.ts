4c3fe9b44d7ad07aa4f41a56845b6e4cd4d3a0cf
oajh04 <cloudtubekr@naver.com>
2021-10-14 20:22:30 +0900

    ⚡️ :: Chat style

--- src/components/Chat/styles.ts
+++ src/components/Chat/styles.ts
@@ -1,2 +1,7 @@
-import styled from "@emotion/styled/types/base";
+import styled from "@emotion/styled";
 
+export const ChatWrapper = styled.div`
+    width: 100%;
+    max-width: 500px;
+    margin: 0 auto;
+`
