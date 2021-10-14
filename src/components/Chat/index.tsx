cd02bf07eacb691f18968373c19dd9c1539f20e3
oajh04 <cloudtubekr@naver.com>
2021-10-14 20:22:21 +0900

    ⚡️ :: Chat index

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import ChatInput from './ChatInput';
+import ChatTitle from './ChatTitle';
+import * as S from './styles'
 
 const Chat = () => {
   return (
     <>
+    <S.ChatWrapper>
+      <ChatTitle />
+      <ChatInput />
+    </S.ChatWrapper>
     </>
   );
 }
