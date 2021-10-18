fafc80185768fecde26eb8c648edc885d12d6bff
oajh04 <cloudtubekr@naver.com>
2021-10-18 10:51:25 +0900

    ⚡️ :: ChatContent Partner

---
+++ src/components/Chat/ChatContent/Partner/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+const PartnerChat = () => {
+  return (
+    <>
+        <PartnerChatWrapper>
+          <PartnerChatBox>
+            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+          </PartnerChatBox>
+          <PartnerChatDate>
+            오후 8시 30분
+          </PartnerChatDate>
+        </PartnerChatWrapper>
+    </>
+  );
+}
+
+export default PartnerChat;
+
+export const PartnerChatWrapper = styled.div`
+  clear:both;
+  float:left;
+  display: flex;
+  margin-top: 10px;
+  margin-left: 10px;
+`
+
+export const PartnerChatBox = styled.div`
+  max-width: 170px;
+  padding: 7px 12px;
+  border-radius: 10px;
+  font-size: 15px;
+  box-sizing: border-box;
+  font-family: 'CookieRunRegular';
+  background-color: #E3E6EA;
+`
+
+export const PartnerChatDate = styled.div`
+  font-size: 11px;
+  font-family: 'CookieRunRegular';
+  margin-left: 5px;
+`
