358dd48cee3d8b6ea08e151c5a853bfcaba55872
oajh04 <cloudtubekr@naver.com>
2021-10-18 10:51:33 +0900

    ⚡️ :: ChatContent style

---
+++ src/components/Chat/ChatContent/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { color } from "../../../styles/Color";
+
+export const ContentWrapper = styled.div`
+    width: 100%;
+    height: calc(100vh - 105px);
+    background-color: white;
+    overflow: scroll;
+    white-space: pre-line;
+    
+`
