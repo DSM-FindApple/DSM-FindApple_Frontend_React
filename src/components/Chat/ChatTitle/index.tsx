2873b4003a883c099c318c508b880c67d99e0db4
oajh04 <cloudtubekr@naver.com>
2021-10-22 15:37:34 +0900

    ⚡️ :: Chat title

--- src/components/Chat/ChatTitle/index.tsx
+++ src/components/Chat/ChatTitle/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import * as S from './styled'
 import { BsArrowLeft,BsThreeDotsVertical } from 'react-icons/bs'
+import { useHistory } from 'react-router';
 
 const ChatTitle = () => {
+  const history = useHistory();
+
+  const onBack = () => {
+    history.goBack()
+  }
+
   return (
     <>
         <S.ChatTitleBox>
-            <BsArrowLeft />
+            <BsArrowLeft onClick={onBack}/>
             <S.ChatPartner>한준호</S.ChatPartner>
             <BsThreeDotsVertical />
         </S.ChatTitleBox>
