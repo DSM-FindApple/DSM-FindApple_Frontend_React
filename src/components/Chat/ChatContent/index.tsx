398efd736d9e8298f565c950b04abbf2fe998ff0
oajh04 <cloudtubekr@naver.com>
2021-10-22 17:55:31 +0900

    ⚡️ :: chat content index

--- src/components/Chat/ChatContent/index.tsx
+++ src/components/Chat/ChatContent/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PartnerChat from './Partner';
 import * as S from './styles'
 
 const ChatContent = () => {
+
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    console.log(vh)
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  },[])
+
+
+  
   return (
     <>
         <S.ContentWrapper>
+            <PartnerChat/>
+            <PartnerChat/>
+            <PartnerChat/>
+            <PartnerChat/>
+            <PartnerChat/>
+            <PartnerChat/>
+            <PartnerChat/>
             <PartnerChat/>
             <PartnerChat/>
             <PartnerChat/>
