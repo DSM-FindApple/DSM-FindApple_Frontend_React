618c9b1500f1b2d6878a1555115dec2911b7960c
oajh04 <cloudtubekr@naver.com>
2021-10-22 15:36:40 +0900

    ⚡️ :: Map header

--- src/components/MainMap/index.tsx
+++ src/components/MainMap/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Map } from 'react-kakao-maps-sdk'
 import KakaoMap from '../KakaoContainer/KakaoMap/KakaoMap';
+import Header from './Header';
 import * as S from './styles'
 
 const MainMap = () => {
   return (
     <>
       <S.Wrapper >
+        <Header/>
         <KakaoMap />
       </S.Wrapper>
     </>
