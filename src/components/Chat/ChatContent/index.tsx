126d1de1855fb5e28508060122f9b2f9643974f3
oajh04 <cloudtubekr@naver.com>
2021-10-27 12:07:45 +0900

    ⚡️ :: 로딩시 스크롤 맨 밑으로

--- src/components/Chat/ChatContent/index.tsx
+++ src/components/Chat/ChatContent/index.tsx
@@ -1,33 +1,46 @@
 import React, { useEffect, useRef } from 'react';
+import MyChat from './Myself';
 import PartnerChat from './Partner';
 import * as S from './styles'
 
 const ChatContent = () => {
 
+  const scrollRef = useRef() as React.MutableRefObject<HTMLInputElement>;;
+  
+  const Scroll = () => {
+    if (scrollRef && scrollRef.current) {
+      scrollRef.current.scrollIntoView({ inline: 'nearest', block: 'end', behavior: 'smooth' });
+    }
+  }
+  
+  useEffect(() => {
+    Scroll()
+  }, [])
+
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
-    console.log(vh)
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
   },[])
-
-
   
   return (
     <>
         <S.ContentWrapper>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
-            <PartnerChat/>
+          <PartnerChat/>
+          <PartnerChat/>
+          <MyChat/>
+          <PartnerChat/>
+          <PartnerChat/>
+          <MyChat/>
+          <PartnerChat/>
+          <PartnerChat/>
+          <MyChat/>
+          <PartnerChat/>
+          <div ref={scrollRef} style={{marginTop: '11px'}}/>
         </S.ContentWrapper>
     </>
   );
