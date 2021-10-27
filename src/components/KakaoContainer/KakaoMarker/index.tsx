d0dc0939b29c39a0bd0d3de8b39a3c843a17d3ef
oajh04 <cloudtubekr@naver.com>
2021-10-27 10:34:59 +0900

    ⚡️ :: Kakao marker type 추가

--- src/components/KakaoContainer/KakaoMarker/index.tsx
+++ src/components/KakaoContainer/KakaoMarker/index.tsx
@@ -1,16 +1,17 @@
 import React, { FC, useState } from 'react';
 import { CustomOverlayMap, MapMarker, useMap } from 'react-kakao-maps-sdk';
-import { marker } from '../../../assets';
-import { InfoMarker } from './InfoMarker';
+import { YellowMarker, BlueMarker } from '../../../assets';
+import InfoMarker from './InfoMarker';
 
 interface Props {
   lat: number,
   lng: number,
   index: number,
   title: string,
+  type: string,
 }
 
-  const KakaoMarker: FC<Props> = ({lat, lng, index, title}) => {
+  const KakaoMarker: FC<Props> = ({lat, lng, index, title, type}) => {
     const map = useMap()
     const [ isShow, setIsShow ] = useState<boolean>(false)
     const [ sizeData, setSizeData ] = useState({width: 49, height: 54})
@@ -29,11 +30,11 @@ interface Props {
       <>
       <MapMarker
         image={{
-          src: `${marker}`, // 마커이미지의 주소입니다
+          src: `${type === 'lost' ? YellowMarker : BlueMarker}`,
           size: {
             width: sizeData.width,
             height: sizeData.height,
-          }, // 마커이미지의 크기입니다
+          },
           
         }}
 
@@ -45,15 +46,14 @@ interface Props {
       />
       {
         isShow &&
-        <CustomOverlayMap // 커스텀 오버레이를 표시할 Container
-          // 커스텀 오버레이가 표시될 위치입니다
+        <CustomOverlayMap
           position={{
             lat: lat,
             lng: lng,
           }}
           yAnchor={1}
         >
-          <InfoMarker onClosrOverView={() => onClosrOverView()}/>
+          <InfoMarker onClosrOverView={() => onClosrOverView()} type={type}/>
         </CustomOverlayMap>
       }
       </>
