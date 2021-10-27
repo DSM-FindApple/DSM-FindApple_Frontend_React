4bf645f321dcb01a0217cc7bec07b4a39f7be07e
oajh04 <cloudtubekr@naver.com>
2021-10-27 10:32:53 +0900

    ⚡️ :: category data

---
+++ src/libs/data/Category.ts
@@ -0,0 +1,36 @@
+import { Accessory, Car, Cellphone, Clothes, Earphone, Electronic, Purse } from "../../assets"
+
+export const category = [
+    {
+        "name": '이어폰',
+        "img" : Earphone
+    },
+    {
+        "name": '휴대폰',
+        "img" : Cellphone
+    },
+    {
+        "name": '전자기기',
+        "img" : Electronic
+    },
+    {
+        "name": '의류',
+        "img" : Clothes
+    },
+    {
+        "name": '악세사리',
+        "img" : Accessory
+    },
+    {
+        "name": '지갑',
+        "img" : Purse
+    },
+    {
+        "name": '자동차',
+        "img" : Car
+    },
+    {
+        "name": '기타',
+        "img" : Earphone
+    },
+]
