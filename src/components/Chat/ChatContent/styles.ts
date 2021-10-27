ac7a061033aa9cdd0ffdaa7a80ea5b4c4792d92f
oajh04 <cloudtubekr@naver.com>
2021-10-27 12:08:18 +0900

    ⚡️ :: chat content 추가

--- src/components/Chat/ChatContent/styles.ts
+++ src/components/Chat/ChatContent/styles.ts
@@ -3,9 +3,12 @@ import { color } from "../../../styles/Color";
 
 export const ContentWrapper = styled.div`
     width: 100%;
-    height: calc(100vh - 105px);
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100 - 105px);
     background-color: white;
     overflow: scroll;
     white-space: pre-line;
+    display: flex;
+    flex-direction: column;
     
 `
