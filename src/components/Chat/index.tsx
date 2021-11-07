4ce2def542fa8a9d2d7d3cb9d088eed6a45c6f25
oajh04 <cloudtubekr@naver.com>
2021-11-07 23:22:41 +0900

    ⚡️ :: 쿼리 추가

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import ChatContent from './ChatContent';
 import ChatInput from './ChatInput';
 import ChatTitle from './ChatTitle';
 import * as S from './styles'
+import queryString from "query-string";
+import { RouteComponentProps } from 'react-router';
 
-const Chat = () => {
+const Chat: FC<RouteComponentProps> = ({location}: any) => {
+  
+  const query = queryString.parse(location.search);
+  console.log(query.id)
+
   return (
     <>
     <S.ChatWrapper>
