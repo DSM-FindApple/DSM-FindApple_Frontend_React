a3d7f2f799f7037111a1a737f8235f9afc44c7f3
oajh04 <cloudtubekr@naver.com>
2021-11-16 23:21:38 +0900

    ⚡️ :: 분할

---
+++ src/components/LocationList/Location/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import LocationApi from '../../../libs/api/Location/LocationApi';
+import { useInfiniteScroll } from '../../../libs/hooks/useInfiniteScroll';
+import * as S from '../styles'
+
+const Location = ({keyword}: any) => {
+    const [data, loading, last] = useInfiniteScroll((page)=>LocationApi.getLocationList(keyword, page), keyword)
+
+    return (
+        <>
+            {
+                data.map((i: any, index: any) => {
+                    let category= i.category_name.split(' > ')
+                    return (
+                        <S.LocationBox key={i.id+'+'+index}>
+                            <S.PlaceName>
+                                {i.place_name}
+                                <span>{category[category.length-1]}</span>
+                            </S.PlaceName>
+                            <S.Address>{i.address_name}</S.Address>
+                            <S.PhoneNumber>{i.phone}</S.PhoneNumber>
+                        </S.LocationBox>
+                    )
+                })
+            }
+            {
+                loading && <div>loading</div>
+            }
+        </>
+    );
+}
+
+export default Location;
