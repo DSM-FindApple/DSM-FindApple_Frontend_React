1b6d417be2aba50ac71a6a13ab8bade63b224a62
oajh04 <cloudtubekr@naver.com>
2021-11-17 12:04:57 +0900

    ⚡️ :: 지도 이동할때 지도정보 가져오기

--- src/components/KakaoContainer/KakaoMap/index.tsx
+++ src/components/KakaoContainer/KakaoMap/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk'
 
 interface Props {
@@ -6,20 +6,41 @@ interface Props {
 }
 
 const KakaoMap: FC<Props> = ({children}) => {
+  const [ mapData, setMapData] = useState<any>([])
+  const onCenter = (level: number, center: any, nePath: any, swPath: any ) => {
+    setMapData({
+        ...mapData, 
+        center: {
+            lat: center.Ma, 
+            lng: center.La
+        }, 
+        level: level,
+        swLatLng: {
+            lat: swPath.Ma,
+            lng: swPath.La,
+          },
+        neLatLng: {
+            lat: nePath.Ma,
+            lng: nePath.La,
+        },
+    })
+}
 
     return (
         <Map // 지도를 표시할 Container
-        center={{
-          // 지도의 중심좌표
-          lat: 36.39155703543644, 
-          lng: 127.3633693928144,
-        }}
-        style={{
-          // 지도의 크기
-          width: "100vw",
-          height: "100vh",
-        }}
-        level={3} // 지도의 확대 레벨
+          center={{
+            // 지도의 중심좌표
+            lat: 36.39155703543644, 
+            lng: 127.3633693928144,
+          }}
+          style={{
+            // 지도의 크기
+            width: "100vw",
+            height: "100vh",
+          }}
+          level={3} // 지도의 확대 레벨
+          onDragEnd={(map) => onCenter(map.getLevel(), map.getCenter(), map.getBounds().getNorthEast(), map.getBounds().getSouthWest()) }
+          onZoomChanged={(map) => onCenter(map.getLevel(), map.getCenter(), map.getBounds().getNorthEast(), map.getBounds().getSouthWest()) }
         >
             {children}
         </Map>
