f5371c3fbd559f730a5d8f851246764c8a5a4c1b
oajh04 <cloudtubekr@naver.com>
2021-11-18 15:59:40 +0900

    ⚡️ :: ChatInput style

--- src/components/Chat/ChatInput/styles.ts
+++ src/components/Chat/ChatInput/styles.ts
@@ -1,6 +1,28 @@
 import styled from "@emotion/styled";
 import { color } from "../../../styles/Color";
 
+export const ImgBox = styled.div`
+    max-width: 500px;
+    width: 100%;
+    height: 200px;
+    background-color: rgb(0,0,0,0.3);
+    position: fixed;
+    bottom: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    svg{
+        width: 20px;
+        height: 20px;
+        padding: 8px;
+    }
+    img{
+        padding: 10px;
+        float: right;
+        height: 140px;
+    }
+`
+
 export const ChatInputBox = styled.div`
     height: 60px;
     width: 100%;
@@ -17,12 +39,21 @@ export const InputBox = styled.div`
     align-items: center;
     background-color: white;
     border-radius: 10px;
+`
+
+export const FileLabel = styled.label`
+    margin: 0 9px;
+    width: 30px;
+    height: 25px;
     svg{
-        margin: 0 9px;
-        width: 30px;
-        height: 25px;
+        width: 100%;
+        height: 100%;
         cursor: pointer;
     }
+`
+
+export const FileInput = styled.input`
+    display: none;
 `
 
 export const ChatInput = styled.input`
@@ -48,3 +79,4 @@ export const SendChatButton = styled.button`
         height: 28px;
     }
 `
+
