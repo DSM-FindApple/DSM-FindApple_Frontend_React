58641086e7533b314f6faf895e3b3ce1729c8972
oajh04 <cloudtubekr@naver.com>
2021-11-18 16:00:29 +0900

    ⚡️ :: Chat

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,12 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import ChatContent from './ChatContent';
 import ChatInput from './ChatInput';
 import ChatTitle from './ChatTitle';
 import * as S from './styles'
 import queryString from "query-string";
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps, useHistory } from 'react-router';
+import styled from '@emotion/styled';
 
 const Chat: FC<RouteComponentProps> = ({location}: any) => {
+  const history = useHistory()
+
+  useEffect(() => {
+
+  },[])
+
+  const LocationChoice = (date: string, id: string) => {
+    history.push({
+      pathname: "/location",
+      state: {date: date, id: id}
+    });
+  }
   
   const query = queryString.parse(location.search);
   console.log(query.id)
