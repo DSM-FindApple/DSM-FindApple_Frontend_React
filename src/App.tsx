0e547eec011a7afcc455057c4d98b2e77473ec20
oajh04 <cloudtubekr@naver.com>
2021-11-19 10:43:26 +0900

    ⚡️ :: Location

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import './App.css';
 import { GlobalStyle } from './styles/GlobalStyles';
 import RootRouter from './Router/RootRouter';
 import {Global} from '@emotion/react'
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 function App() {
+  const history = useHistory()
+  useEffect(() => {
+    window.addEventListener('LocationChoice', async (e) => {
+      history.push('/location')
+    })
+    window.addEventListener('android', async (e) => {
+      history.push('/location')
+    })    
+  },[])
+
+  const LocationChoice = () => {
+    history.push('/location')
+  }
+
   return (
     <>
       <Global styles={GlobalStyle} />
