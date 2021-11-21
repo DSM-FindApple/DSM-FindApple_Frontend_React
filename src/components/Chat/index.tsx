76140aca75022934b472d8e7a59a958ceb26ada2
oajh04 <cloudtubekr@naver.com>
2021-11-21 23:17:54 +0900

    ⚡️ :: socket 연결

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,36 +1,37 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ChatContent from './ChatContent';
 import ChatInput from './ChatInput';
 import ChatTitle from './ChatTitle';
 import * as S from './styles'
 import queryString from "query-string";
 import { RouteComponentProps, useHistory } from 'react-router';
+import io from 'socket.io-client';
+import { useSocket } from '../../libs/hooks/useSocket';
 
 const Chat: FC<RouteComponentProps> = ({location}: any) => {
-  const history = useHistory()
+  const query = queryString.parse(location.search);
+  const [messages, setMessages] = useState<any[]>([]);
+  const {socket} = useSocket()
+  const roomid = '0fecda03-3de7-4c63-89c2-743355b6f9a7'
 
   useEffect(() => {
-    window.addEventListener('LocationChoice', async (e) => {
-      history.push('/location')
-    })
-    window.addEventListener('android', async (e) => {
-      history.push('/location')
-    })    
-  },[])
+    console.log(messages)
+    socket.current.on("message", (message: any) => {
+      setMessages([...messages, message])
+      console.log(message)
+    });
+  },[messages]);
 
-  const LocationChoice = () => {
-    history.push('/location')
+  const onDisConnect = () => {
+    socket.current.emit("disconnect")
   }
-  
-  const query = queryString.parse(location.search);
-  console.log(query.id)
 
   return (
     <>
     <S.ChatWrapper>
       <ChatTitle />
-      <ChatContent />
-      <ChatInput />
+      <ChatContent data={messages}/>
+      <ChatInput chatId={roomid}/>
     </S.ChatWrapper>
     </>
   );
