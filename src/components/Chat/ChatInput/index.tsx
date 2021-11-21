265d38dd737df8ca11738b1d2549393b5940d601
oajh04 <cloudtubekr@naver.com>
2021-11-21 23:17:27 +0900

    ⚡️ :: socket 채팅 보내기

--- src/components/Chat/ChatInput/index.tsx
+++ src/components/Chat/ChatInput/index.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 import * as S from './styles'
 import { BsImage } from 'react-icons/bs'
 import { AiOutlineSend } from 'react-icons/ai'
 import { GrClose } from 'react-icons/gr'
+import { useSocket } from '../../../libs/hooks/useSocket';
 
-const ChatInput = () => {
+interface Props {
+  chatId: string;
+}
+
+const ChatInput:FC<Props> = ({chatId}) => {
+  const {socket} = useSocket();
+  const [ message, setMessage ] = useState<string>('');
   const [fileURL, setFileURL] = useState<any>(null)
   const [isShow, setIsShow ] = useState<boolean>(false)
 
@@ -14,6 +21,17 @@ const ChatInput = () => {
 
   const onGetImage = () => {
     (window as any).ChatDetail.startGetImage();
+  }
+
+  const sendMessage = (e: any) => {
+    e.preventDefault()
+    if(message===''){
+      alert('채팅을 입력해주세요')
+    } else {
+      socket.current.emit('sendMessage', {
+        chatId, message
+      }, setMessage(''))
+    }
   }
 
 
@@ -26,10 +44,10 @@ const ChatInput = () => {
           <img src={fileURL} />
         </S.ImgBox>
       }
-      <S.ChatInputBox>
+      <S.ChatInputBox onSubmit={sendMessage}>
         <S.InputBox>
           <BsImage onClick={onGetImage}/>
-          <S.ChatInput />
+          <S.ChatInput onChange={(e: any) => setMessage(e.target.value)} value={message}/>
         </S.InputBox>
         <S.SendChatButton>
           <AiOutlineSend/>
