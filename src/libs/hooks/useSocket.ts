9b2ea20b393e98b9d04d55d9e2ecc4b30f607714
oajh04 <cloudtubekr@naver.com>
2021-11-21 23:18:49 +0900

    ⚡️ :: useSocket

---
+++ src/libs/hooks/useSocket.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from 'react';
+import socketIO from 'socket.io-client';
+
+const ENDPOINT= `http://3.35.174.250:8080?token=${localStorage.getItem('access-token')}`
+
+export const useSocket = () => {
+  const socket = useRef<any>();
+
+  useEffect(() => {
+    socket.current = socketIO.connect(ENDPOINT, {
+      transports: ['websocket'],
+    });
+  }, []);
+
+  return {socket} as const;
+};
