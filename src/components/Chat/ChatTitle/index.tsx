cee85e00782ae74e328db5ab1fc52c0d5f2ee184
oajh04 <cloudtubekr@naver.com>
2021-11-23 23:26:16 +0900

    ⚡️ :: 채팅 유저 차단

--- src/components/Chat/ChatTitle/index.tsx
+++ src/components/Chat/ChatTitle/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled'
 import { BsArrowLeft } from 'react-icons/bs'
 import { AiFillSchedule } from 'react-icons/ai'
 import { useHistory } from 'react-router';
 import { useRecoilValue } from 'recoil';
+import { GoTriangleDown, GoTriangleUp } from 'react-icons/go'
 import { chatState } from '../../../Recoil/chat/chatState';
+import chatApi from '../../../libs/api/chat/chatApi';
 
 const ChatTitle = () => {
   const chatUserState = useRecoilValue(chatState);
+  const [ isOpenSetting, setIsOpenSetting ] = useState<boolean>(false)
   const history = useHistory()
 
   const onSelectDate = () => {
@@ -19,11 +22,58 @@ const ChatTitle = () => {
     (window as any).ChatDetail.exitChatDetail();
   }
 
+  const onSetting = () => {
+    setIsOpenSetting(!isOpenSetting)
+  }
+
+  const onChatBan = (type: string) => {
+    if(type === "post"){
+      chatApi.postChatBan(chatUserState.targetId)
+      .then((res) => {
+        alert("해당 유저를 차단했습니다.")
+        setIsOpenSetting(true);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    } else if(type==="delete"){
+      chatApi.deleteChatBan(chatUserState.targetId)
+      .then((res) => {
+        alert("해당 유저를 차단을 해제했습니다.")
+        setIsOpenSetting(false);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+
   return (
     <>
         <S.ChatTitleBox>
             <BsArrowLeft onClick={onBack}/>
-            <S.ChatPartner>{chatUserState.title}</S.ChatPartner>
+            <S.ChatPartner>
+              <div onClick={onSetting}>{chatUserState.title}</div>
+              {
+                isOpenSetting ? 
+                <GoTriangleDown/>
+                :
+                <GoTriangleUp/>
+              }
+              
+            </S.ChatPartner>
+              {
+                isOpenSetting &&
+                <S.ChatSetting>
+                    <div>채팅방 나가기</div>
+                    {
+                      chatUserState.isBan ?
+                      <div onClick={()=>onChatBan("delete")}>상대방 차단해제</div>
+                      : <div onClick={()=>onChatBan("post")}>상대방 차단</div>
+                    }
+                    
+                </S.ChatSetting>
+              }
             <S.Appointment onClick={onSelectDate}>
               <AiFillSchedule />
               <span>약속잡기</span>
