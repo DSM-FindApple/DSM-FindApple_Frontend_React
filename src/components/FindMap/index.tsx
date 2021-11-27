306505e929ced0af2e65c2abc2b792d59979da6a
oajh04 <cloudtubekr@naver.com>
2021-11-27 23:38:14 +0900

    ⚡️ :: FindMap api 호출

--- src/components/FindMap/index.tsx
+++ src/components/FindMap/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import KakaoMarker from '../KakaoContainer/KakaoMarker';
 import KakaoMap from '../KakaoContainer/KakaoMap';
 import Header from '../KakaoContainer/Header';
 import * as S from './styles'
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { fildArticelState, fildArticleSeletor } from '../../Recoil/fildArticleState/fildArticleState';
+import mapApi from '../../libs/api/map/mapApi';
 
 const markerdata = [
   {
@@ -35,26 +36,39 @@ const markerdata = [
 ];
 
 const FindMap = () => {
-  const setFindLatLng = useSetRecoilState(fildArticelState)
-  const fildArticleData = useRecoilValue(fildArticleSeletor)
+  const [ findAritcleData, setFindArticleData ] = useState<any>({
+    startLatitude: 0,
+    endLatitude: 0,
+    startLongitude: 0,
+    endLongitude: 0,
+  });
+  const [ findData, setFindData ] = useState<any>([markerdata]);
 
-  console.log(fildArticleData)
+  useEffect(() => {
+    const {startLatitude, endLatitude, startLongitude, endLongitude} = findAritcleData
+      mapApi.getFind(startLatitude, endLatitude, startLongitude, endLongitude)
+      .then((res) => {
+          console.log(res)
+          setFindData(res.data)
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+  },[ findAritcleData ])
 
 
   return (
     <>
       <S.Wrapper >
         <Header/>
-        <KakaoMap setLatLng={setFindLatLng}>
+        <KakaoMap setLatLng={setFindArticleData}>
           {
-                markerdata.map((data, index) => (
+                findData.map((data: any, index: number) => (
                     <>
                         <KakaoMarker
                             type="find"
-                            lat={data.lat} 
-                            lng={data.lng} 
+                            {...data} 
                             index={index}
-                            title={data.title}
                             key={`EventMarkerContainer-${data.lat}-${data.lng}`}
                         />
                     </>
