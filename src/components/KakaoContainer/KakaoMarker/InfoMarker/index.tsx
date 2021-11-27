0fb7a60bce5c1d17eb1325efa7421deef6799104
oajh04 <cloudtubekr@naver.com>
2021-11-27 23:38:45 +0900

    ⚡️ :: image api

--- src/components/KakaoContainer/KakaoMarker/InfoMarker/index.tsx
+++ src/components/KakaoContainer/KakaoMarker/InfoMarker/index.tsx
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FC } from 'react';
 import * as S from './styles'
 import { GrClose } from 'react-icons/gr'
+import imageApi from '../../../../libs/api/image/imageApi';
 
 interface Props {
   onClosrOverView: () => void,
   type: string,
+  category: string
+  detail: string
+  findAt: string
+  findId: number
+  findImages: string[]
+  findUser: string
+  kakaoId: number
+  latitude: number
+  longitude: number
+  profileUrl: string
+  title: string
+  topComment: string | null
+  writeAt: string
+  index: number
 }
 
+const InfoMarker: FC<Props> = (props) => {
+  const [img, setImg] = useState<any>('')
+  const {
+    type,
+    category,
+    detail,
+    findAt,
+    findId,
+    findImages,
+    findUser,
+    kakaoId,
+    latitude,
+    longitude,
+    profileUrl,
+    title,
+    topComment,
+    writeAt,
+    onClosrOverView,
+  } = props;
 
-const InfoMarker: FC<Props> = ({onClosrOverView, type}) => {
+  useEffect(() => {
+    imageApi.getImage(findImages[0])
+    .then((res) => {
+      setImg(atob(unescape(encodeURIComponent(res.data))))
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [findImages])
+  
   return (
     <>
       <S.InfoMarkerWrapper type={type}>
           <S.InfoHeader>
-            <S.InfoTitle>홈팟 잃어버리신분?</S.InfoTitle>
+            <S.InfoTitle>{title}</S.InfoTitle>
             <GrClose width='25px' onClick={onClosrOverView}/>  
           </S.InfoHeader>
-          <S.InfoAddress>대전광역시 유성구 가정북로 76</S.InfoAddress>
+          <S.InfoAddress>{detail}</S.InfoAddress>
+          <img src={findImages[0]} alt="사진"/>
       </S.InfoMarkerWrapper>
     </>
   )
