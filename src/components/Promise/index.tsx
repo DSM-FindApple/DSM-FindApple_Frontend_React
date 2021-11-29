d81b302722d628a62c660c83bd2f4f4cd09941e8
oajh04 <cloudtubekr@naver.com>
2021-11-29 23:42:27 +0900

    ⚡️ :: test

--- src/components/Promise/index.tsx
+++ src/components/Promise/index.tsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Promise = () => {
+  const [ number, setNumber ] = useState(0);
 
   useEffect(() => {
     window.addEventListener("testAlert", async (e) => {
       alert('asdaedjaoijdoia')
-  });
-
-  },[])
+    })
+    window.addEventListener("numberAdd", async (e) => {
+      alert('numberAdd')
+    })
+  },[]);
 
   function testAlert() {
       alert('test')
   }
 
+  const NumberAdd = () => {
+    setNumber(number+1)
+  }
+
   return (
     <>
         <button onClick={testAlert} style={{float: 'right'}}>testAlert</button>
+        <div style={{position: 'absolute', top: '50%', left:'50%', transform: 'translate(-50%, -50%)', display: 'flex', flexDirection: 'column'}}>
+          <div style={{float: 'right'}}>{number}</div>
+          <button onClick={NumberAdd} style={{float: 'right'}}>numberAdd</button>
+        </div>
     </>
   );
 }
