b95cb09cdf3e6bcb743419ead3192e5dc9f43e04
oajh04 <cloudtubekr@naver.com>
2021-12-03 23:08:53 +0900

    ⚡️ :: Android 통신

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -6,16 +6,27 @@ import * as S from './styles'
 import queryString from "query-string";
 import { RouteComponentProps, useHistory } from 'react-router';
 import { useSocket } from '../../libs/hooks/useSocket';
-import { useRecoilState } from 'recoil';
-import { chatMessageState } from '../../Recoil/chat/chatState';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { chatMessageState, chatState } from '../../Recoil/chat/chatState';
 
 const Chat: FC<RouteComponentProps> = ({location}: any) => {
   const query = queryString.parse(location.search);
   const history = useHistory()
+  const setChatUserState = useSetRecoilState(chatState)
   const [chatMessage, setChatMessage ] = useRecoilState(chatMessageState)
   const {socket} = useSocket();
   // const [ chatlist, setChatlist ] = useState(chatMessage)
   let chatlist: any[] = chatMessage;
+
+  (window as any).chatInfo = function(chatId: string, isBan: boolean, title: string, topMessage: string, targetId: number) {
+    setChatUserState({
+      chatId: chatId,
+      isBan: isBan,
+      title: title,
+      topMessage: topMessage,
+      targetId: targetId
+    })
+  }
 
   useEffect(()=> {
     socket.current.on("connect", ()=> {
