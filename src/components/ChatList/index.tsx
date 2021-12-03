bb258d77cfc6007e6a59ae98b413145d866a46cb
oajh04 <cloudtubekr@naver.com>
2021-12-03 22:54:42 +0900

    ⚡️ :: chatlist

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -4,11 +4,17 @@ import ChatListContainer from './Container/ChatListContainer';
 import * as S from './styles'
 import io from 'socket.io-client';
 import chatApi from '../../libs/api/chat/chatApi';
+import { useRecoilValue } from 'recoil';
+import { tokenState } from '../../Recoil/auth/authApi';
 
 
 const ChatList = () => {
   const [ data, setData ] = useState([]);
+  const token = useRecoilValue(tokenState)
+
   useEffect(() => {
+    console.log(token, 'asd')
+    console.log(localStorage.getItem('access-token'), 'asd')
     chatApi.getChatList()
     .then((res) => {
       setData(res.data)
@@ -17,7 +23,7 @@ const ChatList = () => {
     .catch((err) => {
       console.log(err)
     })
-  },[])
+  },[token])
 
   return (
     <>
