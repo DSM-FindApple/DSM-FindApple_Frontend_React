f199ab051b615fc3840b12cd2b58e34caf5be73d
oajh04 <cloudtubekr@naver.com>
2021-12-05 01:17:17 +0900

    ⚡️ :: FindMap

--- src/components/FindMap/index.tsx
+++ src/components/FindMap/index.tsx
@@ -7,34 +7,6 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { fildArticelState, fildArticleSeletor } from '../../Recoil/fildArticleState/fildArticleState';
 import mapApi from '../../libs/api/map/mapApi';
 
-const markerdata = [
-  {
-    title: "대전역",
-    lat: 36.33248155546412,
-    lng: 127.4342002693787,
-  },
-  {
-    title: "천리집",
-    lat: 36.39496683963465,
-    lng: 127.35156630094322,
-  },
-  {
-    title: "감동헤어",
-    lat: 36.38941522030629,
-    lng: 127.34884929417515,
-  },
-  {
-    title: "대덕대학교",
-    lat: 36.390919558287464, 
-    lng: 127.36545386937999,
-  },
-  {
-    title: "금성초 병설",
-    lat: 36.391286658188996, 
-    lng: 127.34983550570026, 
-  },
-];
-
 const FindMap = () => {
   const [ findAritcleData, setFindArticleData ] = useState<any>({
     startLatitude: 0,
@@ -42,7 +14,7 @@ const FindMap = () => {
     startLongitude: 0,
     endLongitude: 0,
   });
-  const [ findData, setFindData ] = useState<any>([markerdata]);
+  const [ findData, setFindData ] = useState<any>([]);
 
   useEffect(() => {
     const {startLatitude, endLatitude, startLongitude, endLongitude} = findAritcleData
