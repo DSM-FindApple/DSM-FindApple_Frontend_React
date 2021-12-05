a9ac4232d71756e47cca1a0fa401e12f5ff1b942
oajh04 <cloudtubekr@naver.com>
2021-12-05 14:55:44 +0900

    ⚡️ :: 이미지 제거

--- src/components/KakaoContainer/KakaoMarker/InfoMarker/index.tsx
+++ src/components/KakaoContainer/KakaoMarker/InfoMarker/index.tsx
@@ -33,22 +33,6 @@ const InfoMarker: FC<Props> = (props) => {
     onClosrOverView,
   } = props;
 
-  useEffect(() => {
-    console.log(`${process.env.REACT_APP_BASE_URL}/${findImages[0]}`)
-    imageApi.getImage(findImages[0])
-    .then((res) => {
-      let bytes, blob;
-      bytes = new Uint8Array(res.data);
-      blob = new Blob([bytes], {type:'image/*'});
-      let url = window.URL || window.webkitURL;
-      let imgsrc = url.createObjectURL(blob);
-      setImg(imgsrc);
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }, [findImages])
-
   console.log(img)
   
   return (
@@ -59,7 +43,6 @@ const InfoMarker: FC<Props> = (props) => {
             <GrClose width='25px' onClick={onClosrOverView}/>  
           </S.InfoHeader>
           <S.InfoAddress>{detail}</S.InfoAddress>
-          <img src={`${img}`} alt="사진"/>
       </S.InfoMarkerWrapper>
     </>
   )
