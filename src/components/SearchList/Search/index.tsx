7a90584ab5c9d455923cf38c708965de9a0cedaa
oajh04 <cloudtubekr@naver.com>
2021-12-05 13:02:14 +0900

    ⚡️ :: 클릭시 이동

--- src/components/SearchList/Search/index.tsx
+++ src/components/SearchList/Search/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { useSetRecoilState } from 'recoil';
 import mapApi from '../../../libs/api/map/mapApi';
 import { useInfiniteScroll } from '../../../libs/hooks/useInfiniteScroll';
+import { mapState } from '../../../Recoil/map/mapState';
 import * as S from '../styles'
 
 const Search = ({keyword}: any) => {
+    const history = useHistory()
     const geocoder = new kakao.maps.services.Geocoder();
     const [data, loading, _] = useInfiniteScroll((page)=>mapApi.getFindTitleSearch(keyword, page), keyword, -1)
+    const setCenter = useSetRecoilState(mapState)
+
+    const onSearch = (lat: number, lng: number) => {
+        setCenter({
+            lat: lat,
+            lng: lng
+        })
+        history.goBack()
+    }
 
     return (
         <>
@@ -19,7 +32,7 @@ const Search = ({keyword}: any) => {
                         }  
                     })
                     return (
-                        <S.SearchBox key={i.kakaoId+'+'+index}>
+                        <S.SearchBox key={i.kakaoId+'+'+index} onClick={() => onSearch(i.latitude, i.longitude)}>
                             <S.PlaceName>
                                 {i.title}
                             </S.PlaceName>
